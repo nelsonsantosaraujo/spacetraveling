8fc8ee5871be561f007a6e4e88d0fa0c01738a7d
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-31 16:26:43 -0300

    Add Utteranc repo env variable

--- src/components/UtterancesComments/index.tsx
+++ src/components/UtterancesComments/index.tsx
@@ -9,7 +9,10 @@ const UtterancesComments: React.FC = () => {
         scriptElem.src = 'https://utteranc.es/client.js';
         scriptElem.async = true;
         scriptElem.crossOrigin = 'anonymous';
-        scriptElem.setAttribute('repo', 'nelsonsantosaraujo/spacetraveling');
+        scriptElem.setAttribute(
+          'repo',
+          process.env.NEXT_PUBLIC_UTTERANC_GITHUB_REPO
+        );
         scriptElem.setAttribute('issue-term', 'pathname');
         scriptElem.setAttribute('label', 'blog-comment');
         scriptElem.setAttribute('theme', 'photon-dark');
