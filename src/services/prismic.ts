4568b59ea3c48a2b2cbeacd6f74a8787fe242cb1
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-04-01 07:52:47 -0300

    fix prismic env variable

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,7 +2,7 @@ import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
     req,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
