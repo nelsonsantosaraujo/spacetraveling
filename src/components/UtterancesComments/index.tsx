bb8812ef320da29826cfab288b245d6d981ad584
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-04-01 14:57:08 -0300

    transform Utterac componnet to functional componnet

--- src/components/UtterancesComments/index.tsx
+++ src/components/UtterancesComments/index.tsx
@@ -1,25 +1,23 @@
-const UtterancesComments: React.FC = () => {
-  return (
-    <section
-      ref={element => {
-        if (!element) {
-          return;
-        }
-        const scriptElem = document.createElement('script');
-        scriptElem.src = 'https://utteranc.es/client.js';
-        scriptElem.async = true;
-        scriptElem.crossOrigin = 'anonymous';
-        scriptElem.setAttribute(
-          'repo',
-          process.env.NEXT_PUBLIC_UTTERANC_GITHUB_REPO
-        );
-        scriptElem.setAttribute('issue-term', 'pathname');
-        scriptElem.setAttribute('label', 'blog-comment');
-        scriptElem.setAttribute('theme', 'photon-dark');
-        element.appendChild(scriptElem);
-      }}
-    />
-  );
-};
+import { useEffect, useRef } from 'react';
 
-export default UtterancesComments;
+export default function UtterancesComments(): JSX.Element {
+  const commentsDiv = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (commentsDiv) {
+      const scriptEl = document.createElement('script');
+      scriptEl.setAttribute('src', 'https://utteranc.es/client.js');
+      scriptEl.setAttribute('crossorigin', 'anonymous');
+      scriptEl.setAttribute('async', 'true');
+      scriptEl.setAttribute(
+        'repo',
+        process.env.NEXT_PUBLIC_UTTERANC_GITHUB_REPO
+      );
+      scriptEl.setAttribute('issue-term', 'pathname');
+      scriptEl.setAttribute('theme', 'photon-dark');
+      commentsDiv.current.appendChild(scriptEl);
+    }
+  }, []);
+
+  return <div ref={commentsDiv} />;
+}
