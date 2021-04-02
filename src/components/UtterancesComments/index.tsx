0f2d1554dfa73281776b634ca0886c080f0c1359
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-04-02 10:59:20 -0300

    Fix Utterances error on loading comments after navigation between posts

--- src/components/UtterancesComments/index.tsx
+++ src/components/UtterancesComments/index.tsx
@@ -1,23 +1,64 @@
-import { useEffect, useRef } from 'react';
+// Code by:
+// https://www.gregorygaines.com/blog/posts/2020/11/21/how-to-integrate-utterances-in-react-with-reloading-comments
+import { useEffect } from 'react';
 
-export default function UtterancesComments(): JSX.Element {
-  const commentsDiv = useRef<HTMLDivElement>();
+const addUtterancesScript = (
+  parentElement: HTMLElement,
+  repo: string,
+  label: string,
+  issueTerm: string,
+  theme: string,
+  isIssueNumber: boolean
+): void => {
+  const script = document.createElement('script');
+  script.setAttribute('src', 'https://utteranc.es/client.js');
+  script.setAttribute('crossorigin', 'anonymous');
+  script.setAttribute('async', 'true');
+  script.setAttribute('repo', repo);
+
+  if (label !== '') {
+    script.setAttribute('label', label);
+  }
+
+  if (isIssueNumber) {
+    script.setAttribute('issue-number', issueTerm);
+  } else {
+    script.setAttribute('issue-term', issueTerm);
+  }
+
+  script.setAttribute('theme', theme);
+
+  parentElement.appendChild(script);
+};
+
+const UtterancesComments = (): JSX.Element => {
+  const repo = process.env.NEXT_PUBLIC_UTTERANC_GITHUB_REPO;
+  const theme = 'photon-dark';
+  const issueTerm = 'pathname';
+  const label = 'Comments';
 
   useEffect(() => {
-    if (commentsDiv) {
-      const scriptEl = document.createElement('script');
-      scriptEl.setAttribute('src', 'https://utteranc.es/client.js');
-      scriptEl.setAttribute('crossorigin', 'anonymous');
-      scriptEl.setAttribute('async', 'true');
-      scriptEl.setAttribute(
-        'repo',
-        process.env.NEXT_PUBLIC_UTTERANC_GITHUB_REPO
-      );
-      scriptEl.setAttribute('issue-term', 'pathname');
-      scriptEl.setAttribute('theme', 'photon-dark');
-      commentsDiv.current.appendChild(scriptEl);
-    }
-  }, []);
+    // Get comments box
+    const commentsBox = document.getElementById('commentsBox');
 
-  return <div ref={commentsDiv} />;
-}
+    // Check if comments box is loaded
+    if (!commentsBox) {
+      return;
+    }
+
+    // Get utterances
+    const utterances = document.getElementsByClassName('utterances')[0];
+
+    // Remove utterances if it exists
+    if (utterances) {
+      utterances.remove();
+    }
+
+    // Add utterances script
+    addUtterancesScript(commentsBox, repo, label, issueTerm, theme, false);
+  });
+
+  return <div id="commentsBox" />;
+};
+
+export default UtterancesComments;
